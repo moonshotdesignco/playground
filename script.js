d1088e5c7ea975f821322621b90c75ece5b8ebcd
Will <moonshotdesignco@gmail.com>
2020-02-18 21:11:25 -0500

    experimenting with js + sass

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-var d = new Date();
-var copyYear = d.getFullYear();
+function makeHeadH() {
+  var headSec = document.getElementById("header");
+  var h = document.createElement("h1");
+  h.id = "hExternal";
+  h.className = "default big";
+  headSec.appendChild(h);
+}
 
-var headSecText = "head.txt";
-var mainSecText = "main.txt"
-var copySecText = "copy.txt";
+function makeMainPara() {
+  var mainSec = document.getElementById("main");
+  var para = document.createElement("p");
+  para.className = "pExternal default";
+  mainSec.appendChild(para);
+}  
 
-var headSec = document.getElementById("header");
-var h = document.createElement("h1");
-h.innerText = headSecText;
-h.className = "default big";
-headSec.appendChild(h);
+function makeCopyPara() {
+  var copySec = document.getElementById("copyright");
+  var para = document.createElement("p");
+  para.id = "copyExternal";
+  para.className = "default small";
+  copySec.appendChild(para);
+}
 
-var mainSec = document.getElementById("main");
-var para = document.createElement("p");
-para.innerText = mainSecText;
-para.className = "default";
-mainSec.appendChild(para);
+document.onload = makeHeadH();
+document.onload = makeMainPara();
+document.onload = makeMainPara();
+document.onload = makeMainPara();
+document.onload = makeMainPara();
+document.onload = makeCopyPara();
 
-var copySec = document.getElementById("copyright");
-copySec.innerText = copySecText;
-copySec.className = "default small";
+$("#hExternal").load("head.txt");
+$(".pExternal").eq(0).load("main.txt");
+$(".pExternal").eq(1).load("main-sub1.txt");
+$(".pExternal").eq(2).load("main-sub2.txt");
+$(".pExternal").eq(3).load("main-sub3.txt");
+$("#copyExternal").load("copy.txt");
